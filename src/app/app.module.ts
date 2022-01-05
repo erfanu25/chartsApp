016253bf82d04bf4e36c6370ea8d0f85baf04466
erfan <erfan@mislbd.com>
2022-01-05 10:06:01 +0600

    Initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,14 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import {NgApexchartsModule} from 'ng-apexcharts';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent
   ],
-  imports: [
-    BrowserModule
-  ],
+    imports: [
+        BrowserModule,
+        NgApexchartsModule
+    ],
   providers: [],
   bootstrap: [AppComponent]
 })
